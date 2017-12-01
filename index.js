d92ae7d1f48c5f5957d5a216daf46f142694d109
Aleksey Pohodnya <pohodnyaa@gmail.com>
2017-12-01 17:53:18 +0700

    Fixing working with FastBoot 1.0.*

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 
 var fs = require('fs');
 var path = require('path');
+var Funnel = require('broccoli-funnel');
+var Merge = require('broccoli-merge-trees');
+var fastbootTransform = require('fastboot-transform');
+var existSync = require('exists-sync');
 
 module.exports = {
   name: 'ember-cli-jquery-ui',
@@ -11,13 +15,33 @@ module.exports = {
     return path.join(__dirname, 'blueprints');
   },
 
+  treeForVendor(tree) {
+    let trees = [];
+
+    if (tree) {
+      trees.push(tree);
+    }
+
+    const assetDir = path.join(app.bowerDirectory, 'jquery-ui');
+
+    if (existSync(assetDir)) {
+      const browserTrees = fastbootTransform(new Funnel(assetDir, {
+        files: ['jquery-ui.js'],
+        destDir: 'jquery-ui'
+      }));
+      trees.push(browserTrees);
+    }
+
+    return new Merge(trees);
+  },
+
   included: function(app) {
     this._super.included(app);
 
     var options = app.options['ember-cli-jquery-ui'] || {};
     var theme = options.theme || "base";
 
-    app.import(path.join(app.bowerDirectory, 'jquery-ui', 'jquery-ui.js'));
+    app.import('vendor/jquery-ui/jquery-ui.js');
 
     if (theme != 'none') {
       var cssFileDir = path.join(app.bowerDirectory, 'jquery-ui', 'themes', theme);
